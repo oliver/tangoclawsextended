512a5144bbbc34cace9da6d94ab74d5d51e938a1
Oliver Gerlich <oliver.gerlich@gmx.de>
2008-03-08 16:09:26 +0100

    initial version

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+all:
+	perl add_tango_icons.pl
---
+++ add_tango_icons.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $tango = '/usr/share/icons/Tango/';
+my $tbtango = './tango_icons_for_thunderbird-0.7.0-tb/';
+my $targetDir = './newIcons/';
+
+my %mapping = (
+'dir_close' => "$tango/16x16/places/folder.png",
+'dir_close_mark' => "",
+'dir_open' => "$tango/16x16/status/folder-open.png",
+'dir_open_mark' => "",
+'inbox_close' => "$tango/16x16/places/folder_home.png",
+'inbox_open' => "$tango/16x16/places/folder_home.png",
+'new' => "$tbtango/messenger/icons/message-mail-new.png",
+'unread' => "$tbtango/messenger/icons/message-mail.png",
+#'clip' => "$tbtango/messenger/icons/attachment-col.png",
+'clip' => "$tbtango/messenger/icons/attachment.png",
+);
+
+
+for (keys(%mapping))
+{
+    my $dstName = $_;
+    my $dstPath = "$targetDir/$dstName.xpm";
+    my $srcPath = $mapping{$_};
+
+    if (!$srcPath)
+    {
+        # ignore
+        next;
+    }
+
+    #print "icon '$dstName' will be taken from '$srcPath'\n";
+
+    if (! -e $srcPath)
+    {
+        #die("source icon '$srcPath' not found");
+        print "source icon '$srcPath' not found - ignoring\n";
+        next;
+    }
+
+    my $cmd = "convert -alpha on '$srcPath' '$dstPath'";
+    #my $cmd = "convert -alpha on '$srcPath' -background red '$dstPath'";
+    #my $cmd = "convert -matte -background red '$srcPath' '$dstPath'";
+    #my $cmd = "convert xc:red '$srcPath' -composite '$dstPath'";
+    print "cmd: '$cmd'\n";
+    `$cmd`;
+}
+
